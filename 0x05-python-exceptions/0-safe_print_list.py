cc411f069f7ebcad4fa179abe76991389330d4a6
Joseph Ofori <josephofori75@gmail.com>
2023-11-20 15:16:12 -0800

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@ def safe_print_list(my_list=[], x=0):
     num =0
     for a in range(x):
         try:
-            print("{}".format(my_list[x],end = '')
-            num += 1      
+            print("{}".format(my_list[a],end = '')
+            num += 1
         except:
             break
 print()
-return(count)
+return (count)
