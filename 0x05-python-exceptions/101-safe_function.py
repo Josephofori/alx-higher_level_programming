ac87ab8d1f8a3eea871d51b12d48813e590c4b0e
Joseph Ofori <josephofori75@gmail.com>
2023-11-20 16:31:35 -0800

    advance task 2

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-import sys
-
 def safe_function(fct, *args):
+    import sys
     try:
         num = fct(*args)
-    except Exception as err:
-        sys.stderr.write("Exception: {}\n".format(err))
+    except Exception as i:
+        sys.stderr.write("Exception: {}\n".format(i))
         num = None
     return (num)
