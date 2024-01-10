3aa58d355605bbebdb77a1a260acfa1115a2864f
Joseph Ofori <josephofori75@gmail.com>
2024-01-10 18:54:00 +0000

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def no_c(my_string):
     l_string = len(my_string)
-    new_string = my_string[:]
+    new_string = list(my_string)
+    string_update = []
     for i in range(l_string):
-        if my_string[i] == 'c' or mystring[i] == 'C':
-            del my_string[i]
-            new_string = my_string.append()
+        if new_string[i] == 'c' or new_string[i] == 'C':
+            del new_string[i]
+            string_update = new_string.append()
         else:
-            new_string = my_string.append()
-    print(my_string)
+            string_update = new_string.append()
+    print(string_update)
