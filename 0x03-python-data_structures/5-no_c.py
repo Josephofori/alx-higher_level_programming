e05737886c27bb9a02d56e3c3bf24c0b24c95f40
Joseph Ofori <josephofori75@gmail.com>
2024-01-10 19:19:14 +0000

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    l_string = len(my_string)
-    new_string = list(my_string)
-    string_update = []
-    for i in range(l_string):
-        if new_string[i] == 'c' or new_string[i] == 'C':
-            del new_string[i]
-        else:
-            string_update = new_string.append(new_string[i])
-    print(string_update)
+    nStr = my_string.translate({ord(i): None for i in 'cC'})
+    return (nStr)
