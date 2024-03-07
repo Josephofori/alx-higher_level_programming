efe0582fc217a1667c70c733ada3956bb7e40783
Joseph Ofori <josephofori75@gmail.com>
2024-03-07 09:01:07 -0800

    task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dir = a_dictionary.copy()
-    keys_list = list(keys_list.keys())
+    keys_list = list(new_dir.keys())
 
     for i in keys_list:
         new_dir[i] *= 2
