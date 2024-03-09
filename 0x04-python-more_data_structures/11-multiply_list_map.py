12c0bbcd50dc3614052dbde41c0dc703ce223d73
Joseph Ofori <josephofori75@gmail.com>
2024-03-09 05:07:14 -0800

    task 11

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    new_list = my_list.copy()
-    mod_list = map((lambda x: x *= number), new_list)
-    return (mod_list)
+    return (list(map((lambda x: x *= number), my_list)))
